a15d4e3d036cb862cb3232e3407872ca43cf56b2
Michelle Blackwell <92054570+mblackwellgca@users.noreply.github.com>
2022-04-10 12:23:48 -0400

    updating with working code

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
